4e3c23d9d08ac20d963ca0e4ec515425ade85b32
Javaro3 <korol20041@gmail.com>
2023-09-25 22:14:40 +0300

    a

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -19,31 +19,12 @@ CREATE TABLE dbo.SupportingDocuments(
 	[Name] [nvarchar](100) NOT NULL,
 	[Description] [text] NULL,)
 
--- create PassportData
-CREATE TABLE dbo.PassportData(
-	[Id] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	[Number] [nchar](9) NOT NULL,
-	[IssueDate] [date] NOT NULL,
-	[Identification] [nchar](14) NOT NULL,)
-ALTER TABLE [dbo].[PassportData]  WITH CHECK ADD  CONSTRAINT [CK__PassportD__Ident__628FA481] CHECK  (([Identification] like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Za-z][0-9][0-9][0-9][A-Za-z][A-Za-z][0-9]'))
-ALTER TABLE [dbo].[PassportData] CHECK CONSTRAINT [CK__PassportD__Ident__628FA481]
-ALTER TABLE [dbo].[PassportData]  WITH CHECK ADD  CONSTRAINT [CK__PassportD__Numbe__619B8048] CHECK  (([Number] like '[A-Za-z][A-Za-z][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'))
-ALTER TABLE [dbo].[PassportData] CHECK CONSTRAINT [CK__PassportD__Numbe__619B8048]
-
 -- create Contracts
 CREATE TABLE dbo.Contracts(
 	[Id] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[Responsibilities] [nvarchar](100) NOT NULL,
 	[StartDeadline] [date] NOT NULL,
 	[EndDeadline] [date] NOT NULL,)
-
--- create Addresses
-CREATE TABLE dbo.Addresses(
-	[Id] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	[City] [nvarchar](50) NOT NULL,
-	[Street] [nvarchar](50) NOT NULL,
-	[House] [nvarchar](50) NOT NULL,
-	[Apartment] [nvarchar](50) NOT NULL,)
 
 -- create AgentTypes
 CREATE TABLE dbo.AgentTypes(
@@ -76,14 +57,17 @@ CREATE TABLE dbo.Clients(
 	[MiddleName] [nvarchar](20) NOT NULL,
 	[Birthdate] [date] NOT NULL,
 	[MobilePhone] [nvarchar](20) NOT NULL,
-	[Address] [int] NOT NULL,
-	[PassportData] [int] NOT NULL,)
-ALTER TABLE [dbo].[Clients]  WITH CHECK ADD  CONSTRAINT [FK_Clients_Addresses] FOREIGN KEY([Address])
-REFERENCES [dbo].[Addresses] ([Id])
-ALTER TABLE [dbo].[Clients] CHECK CONSTRAINT [FK_Clients_Addresses]
-ALTER TABLE [dbo].[Clients]  WITH CHECK ADD  CONSTRAINT [FK_Clients_PassportData] FOREIGN KEY([PassportData])
-REFERENCES [dbo].[PassportData] ([Id])
-ALTER TABLE [dbo].[Clients] CHECK CONSTRAINT [FK_Clients_PassportData]
+	[City] [nvarchar](50) NOT NULL,
+	[Street] [nvarchar](50) NOT NULL,
+	[House] [nvarchar](50) NOT NULL,
+	[Apartment] [nvarchar](50) NOT NULL,
+	[PassportNumber] [nchar](9) NOT NULL,
+	[PassportIssueDate] [date] NOT NULL,
+	[PassportIdentification] [nchar](14) NOT NULL,)
+ALTER TABLE [dbo].[Clients]  WITH CHECK ADD  CONSTRAINT [CK__PassportD__Ident__628FA481] CHECK  (([PassportIdentification] like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][A-Za-z][0-9][0-9][0-9][A-Za-z][A-Za-z][0-9]'))
+ALTER TABLE [dbo].[Clients] CHECK CONSTRAINT [CK__PassportD__Ident__628FA481]
+ALTER TABLE [dbo].[Clients]  WITH CHECK ADD  CONSTRAINT [CK__PassportD__Numbe__619B8048] CHECK  (([PassportNumber] like '[A-Za-z][A-Za-z][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'))
+ALTER TABLE [dbo].[Clients] CHECK CONSTRAINT [CK__PassportD__Numbe__619B8048]
 
 -- create InsuranceCases
 CREATE TABLE dbo.InsuranceCases(
@@ -143,190 +127,6 @@ ALTER TABLE [dbo].[PolicyInsuranceCases] CHECK CONSTRAINT [FK_PolicyInsuranceCas
 GO
 INSERT INTO AgentTypes
 VALUES ('Штатный работник'), ('Совместитель')
-
--- fill PassportData
-GO
-DECLARE @Counter INT = 1;
-WHILE @Counter <= 100
-BEGIN
-    DECLARE @PassportNumber VARCHAR(9) = 
-	      CHAR(65 + RAND()*(26-0))
-		+ CHAR(65 + RAND()*(26-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0));
-
-    DECLARE @RandomIdentification VARCHAR(14) =
-		  CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(65 + RAND()*(26-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(48 + RAND()*(10-0))
-		+ CHAR(65 + RAND()*(26-0))
-		+ CHAR(65 + RAND()*(26-0))
-		+ CHAR(48 + RAND()*(10-0))
-
-    INSERT INTO PassportData (Number, IssueDate, Identification)
-    VALUES (
-        @PassportNumber,
-        DATEADD(DAY, -CAST(CHECKSUM(NEWID()) % 3650 AS INT), GETDATE()),
-        @RandomIdentification
-    );
-
-    SET @Counter = @Counter + 1;
-END;
-
--- fill Addresses
-GO
-DECLARE @StreetNames TABLE (Name NVARCHAR(50))
-    INSERT INTO @StreetNames (Name)
-    VALUES
-        ('Пролетарская улица'),
-        ('Ленинская улица'),
-        ('Гагарина улица'),
-        ('Советская улица'),
-        ('Пушкинская улица'),
-        ('Московская улица'),
-        ('Кировская улица'),
-        ('Парковая улица'),
-        ('Садовая улица'),
-        ('Комсомольская улица'),
-        ('Школьная улица'),
-        ('Жукова улица'),
-        ('Мичурина улица'),
-        ('Свердлова улица'),
-        ('Октябрьская улица'),
-        ('Горького улица'),
-        ('Красноармейская улица'),
-        ('Рабочая улица'),
-        ('Зеленая улица'),
-        ('Трудовая улица'),
-        ('Полярная улица'),
-        ('Красная улица'),
-        ('Строителей улица'),
-        ('Молодежная улица'),
-        ('Центральная улица'),
-        ('Новая улица'),
-        ('Солнечная улица'),
-        ('Заречная улица'),
-        ('Пионерская улица'),
-        ('Речная улица'),
-        ('Восточная улица'),
-        ('Западная улица'),
-        ('Южная улица'),
-        ('Северная улица'),
-        ('Цветочная улица'),
-        ('Лесная улица'),
-        ('Юбилейная улица'),
-        ('Гранитная улица'),
-        ('Маяковского улица'),
-        ('Первомайская улица'),
-        ('Коммунальная улица'),
-        ('Чкалова улица'),
-        ('Горная улица'),
-        ('Сиреневая улица'),
-        ('Сосновая улица'),
-        ('Дружбы улица'),
-        ('Озерная улица'),
-        ('Заводская улица'),
-        ('Вокзальная улица'),
-        ('Партизанская улица'),
-        ('Островская улица'),
-        ('Городская улица'),
-        ('Карла Маркса улица'),
-        ('Железнодорожная улица'),
-        ('Набережная улица'),
-        ('Мирная улица'),
-        ('Севастопольская улица'),
-        ('Колхозная улица'),
-        ('Совхозная улица'),
-        ('Театральная улица'),
-        ('Лермонтова улица'),
-        ('Пушкинская улица'),
-        ('Мичуринская улица'),
-        ('Свердловская улица'),
-        ('Щорса улица'),
-        ('Смирнова улица'),
-        ('Гусарская улица'),
-        ('Петровская улица'),
-        ('Космонавтов улица'),
-        ('Суворова улица'),
-        ('Фрунзе улица'),
-        ('Толстого улица'),
-        ('Горького улица'),
-        ('Шевченко улица'),
-        ('Греческая улица'),
-        ('Воронцовская улица'),
-        ('Скверная улица'),
-        ('Сельская улица'),
-        ('Холодильная улица'),
-        ('Степная улица'),
-        ('Заводская улица'),
-        ('Космическая улица'),
-        ('Речная улица'),
-        ('Парковая улица'),
-        ('Береговая улица'),
-        ('Школьная улица'),
-        ('Соседская улица'),
-        ('Луговая улица'),
-        ('Озерная улица'),
-        ('Красивая улица'),
-        ('Полевая улица'),
-        ('Дачная улица'),
-        ('Живописная улица'),
-        ('Просторная улица'),
-        ('Зеленая улица'),
-        ('Чистая улица'),
-        ('Тихая улица'),
-        ('Песчаная улица');
-   
-DECLARE @CityNames TABLE (Name NVARCHAR(50))
-    INSERT INTO @CityNames (Name)
-    VALUES 
-		('Москва'),
-		('Санкт-Петербург'),
-		('Нью-Йорк'),
-		('Лондон'),
-		('Париж'),
-		('Токио'),
-		('Пекин'),
-		('Берлин'),
-		('Рим'),
-		('Амстердам'),
-		('Мадрид'),
-		('Лос-Анджелес'),
-		('Сидней'),
-		('Дублин'),
-		('Сингапур'),
-		('Рио-де-Жанейро'),
-		('Каир'),
-		('Мумбаи');
-
-    DECLARE @House INT
-    DECLARE @Apartment INT
-    DECLARE @RandomStreet NVARCHAR(50)
-	DECLARE @RandomCity NVARCHAR(50)
-	
-    WHILE (SELECT COUNT(*) FROM dbo.Addresses) < 100
-    BEGIN
-        SELECT TOP 1 @RandomStreet = Name FROM @StreetNames ORDER BY NEWID()
-        SELECT TOP 1 @RandomCity = Name FROM @CityNames ORDER BY NEWID()
-		SET @House = CAST(RAND() * 100 AS INT) + 1
-        SET @Apartment = CAST(RAND() * 20 AS INT) + 1
-
-        INSERT INTO dbo.Addresses(City, Street, House, Apartment)
-        VALUES (@RandomCity, @RandomStreet, @House, @Apartment)
-    END
 
 -- fill Contracts
 GO
@@ -465,8 +265,165 @@ DECLARE @AgentMiddleNames TABLE (Name NVARCHAR(20))
 
 -- fill Clients
 DECLARE @MobilePhone NVARCHAR(20)
+DECLARE @PassportNumber VARCHAR(9)
+DECLARE @RandomIdentification VARCHAR(14)
+DECLARE @StreetNames TABLE (Name NVARCHAR(50))
+    INSERT INTO @StreetNames (Name)
+    VALUES
+        ('Пролетарская улица'),
+        ('Ленинская улица'),
+        ('Гагарина улица'),
+        ('Советская улица'),
+        ('Пушкинская улица'),
+        ('Московская улица'),
+        ('Кировская улица'),
+        ('Парковая улица'),
+        ('Садовая улица'),
+        ('Комсомольская улица'),
+        ('Школьная улица'),
+        ('Жукова улица'),
+        ('Мичурина улица'),
+        ('Свердлова улица'),
+        ('Октябрьская улица'),
+        ('Горького улица'),
+        ('Красноармейская улица'),
+        ('Рабочая улица'),
+        ('Зеленая улица'),
+        ('Трудовая улица'),
+        ('Полярная улица'),
+        ('Красная улица'),
+        ('Строителей улица'),
+        ('Молодежная улица'),
+        ('Центральная улица'),
+        ('Новая улица'),
+        ('Солнечная улица'),
+        ('Заречная улица'),
+        ('Пионерская улица'),
+        ('Речная улица'),
+        ('Восточная улица'),
+        ('Западная улица'),
+        ('Южная улица'),
+        ('Северная улица'),
+        ('Цветочная улица'),
+        ('Лесная улица'),
+        ('Юбилейная улица'),
+        ('Гранитная улица'),
+        ('Маяковского улица'),
+        ('Первомайская улица'),
+        ('Коммунальная улица'),
+        ('Чкалова улица'),
+        ('Горная улица'),
+        ('Сиреневая улица'),
+        ('Сосновая улица'),
+        ('Дружбы улица'),
+        ('Озерная улица'),
+        ('Заводская улица'),
+        ('Вокзальная улица'),
+        ('Партизанская улица'),
+        ('Островская улица'),
+        ('Городская улица'),
+        ('Карла Маркса улица'),
+        ('Железнодорожная улица'),
+        ('Набережная улица'),
+        ('Мирная улица'),
+        ('Севастопольская улица'),
+        ('Колхозная улица'),
+        ('Совхозная улица'),
+        ('Театральная улица'),
+        ('Лермонтова улица'),
+        ('Пушкинская улица'),
+        ('Мичуринская улица'),
+        ('Свердловская улица'),
+        ('Щорса улица'),
+        ('Смирнова улица'),
+        ('Гусарская улица'),
+        ('Петровская улица'),
+        ('Космонавтов улица'),
+        ('Суворова улица'),
+        ('Фрунзе улица'),
+        ('Толстого улица'),
+        ('Горького улица'),
+        ('Шевченко улица'),
+        ('Греческая улица'),
+        ('Воронцовская улица'),
+        ('Скверная улица'),
+        ('Сельская улица'),
+        ('Холодильная улица'),
+        ('Степная улица'),
+        ('Заводская улица'),
+        ('Космическая улица'),
+        ('Речная улица'),
+        ('Парковая улица'),
+        ('Береговая улица'),
+        ('Школьная улица'),
+        ('Соседская улица'),
+        ('Луговая улица'),
+        ('Озерная улица'),
+        ('Красивая улица'),
+        ('Полевая улица'),
+        ('Дачная улица'),
+        ('Живописная улица'),
+        ('Просторная улица'),
+        ('Зеленая улица'),
+        ('Чистая улица'),
+        ('Тихая улица'),
+        ('Песчаная улица');
+   
+DECLARE @CityNames TABLE (Name NVARCHAR(50))
+    INSERT INTO @CityNames (Name)
+    VALUES 
+		('Москва'),
+		('Санкт-Петербург'),
+		('Нью-Йорк'),
+		('Лондон'),
+		('Париж'),
+		('Токио'),
+		('Пекин'),
+		('Берлин'),
+		('Рим'),
+		('Амстердам'),
+		('Мадрид'),
+		('Лос-Анджелес'),
+		('Сидней'),
+		('Дублин'),
+		('Сингапур'),
+		('Рио-де-Жанейро'),
+		('Каир'),
+		('Мумбаи');
+
+DECLARE @House INT
+DECLARE @Apartment INT
+DECLARE @RandomStreet NVARCHAR(50)
+DECLARE @RandomCity NVARCHAR(50)
+ 
 WHILE (SELECT COUNT(*) FROM dbo.Clients) < 100
     BEGIN
+		SET @PassportNumber = 
+	      CHAR(65 + RAND()*(26-0))
+		+ CHAR(65 + RAND()*(26-0))
+		+ CHAR(48 + RAND()*(10-0))
+		+ CHAR(48 + RAND()*(10-0))
+		+ CHAR(48 + RAND()*(10-0))
+		+ CHAR(48 + RAND()*(10-0))
+		+ CHAR(48 + RAND()*(10-0))
+		+ CHAR(48 + RAND()*(10-0))
+		+ CHAR(48 + RAND()*(10-0));
+
+		SET @RandomIdentification =
+			  CHAR(48 + RAND()*(10-0))
+			+ CHAR(48 + RAND()*(10-0))
+			+ CHAR(48 + RAND()*(10-0))
+			+ CHAR(48 + RAND()*(10-0))
+			+ CHAR(48 + RAND()*(10-0))
+			+ CHAR(48 + RAND()*(10-0))
+			+ CHAR(48 + RAND()*(10-0))
+			+ CHAR(65 + RAND()*(26-0))
+			+ CHAR(48 + RAND()*(10-0))
+			+ CHAR(48 + RAND()*(10-0))
+			+ CHAR(48 + RAND()*(10-0))
+			+ CHAR(65 + RAND()*(26-0))
+			+ CHAR(65 + RAND()*(26-0))
+			+ CHAR(48 + RAND()*(10-0))
         SELECT TOP 1 @AgentName = Name FROM @AgentNames ORDER BY NEWID()
 		SELECT TOP 1 @AgentSurname = Name FROM @AgentSurnames ORDER BY NEWID()
 		SELECT TOP 1 @AgentMiddleName = Name FROM @AgentMiddleNames ORDER BY NEWID()
@@ -480,8 +437,12 @@ WHILE (SELECT COUNT(*) FROM dbo.Clients) < 100
 			+ CHAR(48 + RAND()*(10-0))
 			+ CHAR(48 + RAND()*(10-0))
 			+ CHAR(48 + RAND()*(10-0))
-        INSERT INTO dbo.Clients(Name, Surname, MiddleName, Birthdate, MobilePhone, Address, PassportData)
-        VALUES (@AgentName, @AgentSurname, @AgentMiddleName, DATEADD(DAY, -CAST(CHECKSUM(NEWID()) % 3650 AS INT), GETDATE()), @MobilePhone, RAND()*(100)+1, RAND()*(100)+1)
+		SELECT TOP 1 @RandomStreet = Name FROM @StreetNames ORDER BY NEWID()
+        SELECT TOP 1 @RandomCity = Name FROM @CityNames ORDER BY NEWID()
+		SET @House = CAST(RAND() * 100 AS INT) + 1
+        SET @Apartment = CAST(RAND() * 20 AS INT) + 1
+        INSERT INTO dbo.Clients(Name, Surname, MiddleName, Birthdate, MobilePhone, City, Street, House, Apartment, PassportNumber, PassportIssueDate, PassportIdentification)
+        VALUES (@AgentName, @AgentSurname, @AgentMiddleName, DATEADD(DAY, -CAST(CHECKSUM(NEWID()) % 3650 AS INT), GETDATE()), @MobilePhone, @RandomCity, @RandomStreet, @House, @Apartment, @PassportNumber, DATEADD(DAY, -CAST(CHECKSUM(NEWID()) % 3650 AS INT), GETDATE()), @RandomIdentification)
     END
 
 -- fill InsuranceCases
@@ -538,13 +499,13 @@ SELECT
 	c.MiddleName ClintMiddleName,
 	c.Birthdate,
 	c.MobilePhone,
-	ad.City,
-	ad.Street,
-	ad.House,
-	ad.Apartment,
-	pd.Number,
-	pd.IssueDate,
-	pd.Identification,
+	c.City,
+	c.Street,
+	c.House,
+	c.Apartment,
+	c.PassportNumber,
+	c.PassportIssueDate,
+	c.PassportIdentification,
 	p.PolicyTerm,
 	p.PolicyPayment
 FROM
@@ -553,34 +514,67 @@ FROM
 	InsuranceTypes it,
 	Clients c,
 	AgentTypes a,
-	Contracts co,
-	Addresses ad,
-	PassportData pd
+	Contracts co
+
+-- Clients view
+GO
+CREATE VIEW ClientsView
+AS
+SELECT
+    c.Name,
+	c.Surname,
+	c.MiddleName,
+	c.Birthdate,
+	c.MobilePhone,
+	c.City,
+	c.Street,
+	c.House,
+	c.Apartment,
+	c.PassportNumber,
+	c.PassportIssueDate,
+	c.PassportIdentification
+FROM
+    Clients c
+
+-- InsuranceAgents view
+GO
+CREATE VIEW InsuranceAgentsView
+AS
+SELECT
+    ia.Name,
+	ia.Surname,
+	ia.MiddleName,
+	ag.Type,
+	ia.Salary,
+	c.Responsibilities,
+	c.StartDeadline,
+	c.EndDeadline,
+	ia.TransactionPercent
+FROM
+	InsuranceAgents ia,
+	AgentTypes ag,
+	Contracts c
 
 -- CREATE PROCEDURES
--- Insert Addresses
+-- Insert Clients
 GO
-CREATE PROCEDURE InsertAddress
-    @City NVARCHAR(50),
-    @Street NVARCHAR(50),
-    @House NVARCHAR(50),
-    @Apartment NVARCHAR(50)
+CREATE PROCEDURE InsertClients
+	@Name nvarchar(20),
+	@Surname nvarchar(20),
+	@MiddleName nvarchar(20),
+	@Birthdate date,
+	@MobilePhone nvarchar(20),
+	@City nvarchar(50),
+	@Street nvarchar(50),
+	@House nvarchar(50),
+	@Apartment nvarchar(50),
+	@PassportNumber nchar(9),
+	@PassportIssueDate date,
+	@PassportIdentification nchar(14)
 AS
 BEGIN
-    INSERT INTO Addresses (City, Street, House, Apartment)
-    VALUES (@City, @Street, @House, @Apartment);
-END;
-
--- Insert PassportData
-GO
-CREATE PROCEDURE InsertPassportData
-    @Number NVARCHAR(9),
-    @IssueDate DATE,
-    @Identification NVARCHAR(14)
-AS
-BEGIN
-    INSERT INTO PassportData(Number, IssueDate, Identification)
-    VALUES (@Number, @IssueDate, @Identification);
+    INSERT INTO Clients(Name, Surname, MiddleName, Birthdate, MobilePhone, City, Street, House, Apartment, PassportNumber, PassportIssueDate, PassportIdentification)
+    VALUES (@Name, @Surname, @MiddleName, @Birthdate, @MobilePhone, @City, @Street, @House, @Apartment, @PassportNumber, @PassportIssueDate, @PassportIdentification);
 END;
 
 -- Insert InsuranceCases
@@ -595,4 +589,20 @@ AS
 BEGIN
     INSERT INTO InsuranceCases(Client, InsuranceAgent, Date, SupportingDocument, InsurancePayment)
     VALUES (@Client, @InsuranceAgent, @Date, @SupportingDocument, @InsurancePayment);
+END;
+
+-- Insert InsuranceAgents
+GO
+CREATE PROCEDURE InsertInsuranceAgents
+    @Name nvarchar(20),
+	@Surname nvarchar(20),
+	@MiddleName nvarchar(20),
+	@AgentType int,
+	@Salary money,
+	@Contract int,
+	@TransactionPercent float
+AS
+BEGIN
+    INSERT INTO InsuranceAgents(Name, Surname, MiddleName, AgentType, Salary, Contract, TransactionPercent)
+    VALUES (@Name, @Surname, @MiddleName, @AgentType, @Salary, @Contract, @TransactionPercent);
 END;
